548d2584ed402bca19f09936f687ea6fbc212804
sun <annjin12@naver.com>
2023-10-24 22:30:15 +0900

    2023/10/15 수 조작

---
+++ src/main/java/org/example/Algorithm_20231015_3.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Algorithm_20231015_3 {
+    public static void main(String[] args) {
+        int[][] arr = {{0, 1, 2, 3, 4}
+                     , {3, 1, 2, 0, 4}
+                     , {3, 2, 1, 0, 4}
+                     , {3, 4, 1, 0, 2}};
+        int[][] queries = {{0, 3}, {1, 2}, {1, 4}};
+        String result = "";
+
+        for(int i = 0; i < queries.length; i++) {
+            int[] inArr = queries[i];
+            for(int j = 0; j < inArr.length; j++) {
+                
+
+            }
+        }
+    }
+}
