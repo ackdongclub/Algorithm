9384b9694a831b6f3b627f5f75dc1060b516c487
sun <annjin12@naver.com>
2024-04-22 22:42:15 +0900

    2024.04.22 선물문제

---
+++ src/main/java/org/example/Algorithm_20240318.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Algorithm_20240318 {
+
+    public static void main(String[] args) {
+
+        String[] friend = new String[]{"muzi", "ryan", "frodo", "neo"};
+        String[] gift = new String[]{"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
+
+
+
+
+    }
+
+}
